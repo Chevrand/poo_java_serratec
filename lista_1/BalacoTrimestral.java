ce4cb3128ee2ebf0d614a7a9387dc8f3b2327759
Chevrand <wandersonchevrand27@gmail.com>
2022-04-08 14:42:36 -0300

    update lista 1

---
+++ lista_1/BalacoTrimestral.java
@@ -0,0 +1,19 @@
+package br.com.residencia;
+
+import java.text.NumberFormat;
+
+public class BalacoTrimestral {
+	
+	public static void main(String[] args) {
+		/*Declara váriaveis referentes a gastos dos 3 primeiros meses do ano. Em seguida,
+		somas as mesmas e exibe o resultado em uma mensagem.*/
+		float gastosJaneiro = 15000;
+		float gastosFevereiro = 23000;
+		float gastosMarco = 17000;
+		float gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+		
+		System.out.println(NumberFormat.getCurrencyInstance().format(gastosTrimestre));
+		
+	}
+
+}
